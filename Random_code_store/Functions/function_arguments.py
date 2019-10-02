99fe1eb9176d139105b078afa03ba64416288a3f
Tega Ileleji <tegaileleji@tegas-mbp.lan>
2019-10-02 01:07:11 -0700

    Function Argument

---
+++ Random_code_store/Functions/function_arguments.py
@@ -0,0 +1,32 @@
+#Function Arguments
+
+#we have functions with zero arguments. In orther words these are functions with empty parentheses.
+
+#Function that takes in one arguement
+
+def print_with_exclamation(word):
+    print(word + "!")
+
+print_with_exclamation("spam")
+print_with_exclamation("eggs")
+print_with_exclamation("python")
+
+#The arguements are defined inside the parenthesis
+
+#what is the result of the code?
+
+def print_double(x):
+    print(2*x)
+print_double(3)
+
+
+#Arguments 
+
+def print_sum_twice(x,y):
+    print(x+y)
+    print(x+y)
+print_sum_twice(5,8)
+
+
+
+
