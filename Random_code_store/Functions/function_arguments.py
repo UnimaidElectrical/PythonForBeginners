d69f3a131d682bcea54d3fae2c81131d38dfbca2
Tega Ileleji <tegaileleji@tegas-mbp.lan>
2019-10-03 09:11:34 -0700

    New Updates

--- Random_code_store/Functions/function_arguments.py
+++ Random_code_store/Functions/function_arguments.py
@@ -30,3 +30,27 @@ print_sum_twice(5,8)
 
 
 
+#Referencing Function Arguments
+#Function arguments can be used as variables inside the function defination. However, they cannot be referenced outside the function's defination
+#This also applies to other variables inside a function.
+
+def function (variable):
+    variable +=1 
+    print(variable)
+
+function(7)
+print(variable)
+
+#in order words 
+#parameters are the variables in a function defination, and 
+# arguments are the values put into parameters when the functions are called.
+
+
+#Using an if/else statement in a function call
+def even(x):
+    if x%2==0:
+        print("Yes")
+    else:
+        print("No")
+
+#using an if/elif/else statement in a function call 
