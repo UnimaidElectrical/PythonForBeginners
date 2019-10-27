7b542bc7cc79dd79220a6ac70d7a7db6429ebf6f
Tega Ileleji <tegaileleji@tegas-mbp.lan>
2019-10-26 23:36:08 -0700

    Selection Sort Algorithm

---
+++ Algorithms/Sorting Algorithms/selection_sort.py
@@ -0,0 +1,25 @@
+# Selection Sort
+# Selection sort is also quite simple but frequently outperforms bubble sort.
+# With Selection sort, we divide our input list / array into two parts: the sublist 
+# of items already sorted and the sublist of items remaining to be sorted that make up 
+# the rest of the list. 
+# We first find the smallest element in the unsorted sublist and 
+# place it at the end of the sorted sublist. Thus, we are continuously grabbing the smallest 
+# unsorted element and placing it in sorted order in the sorted sublist.
+# This process continues iteratively until the list is fully sorted.
+
+
+def selection_sort(arr):        
+    for i in range(len(arr)):
+        minimum = i
+        
+        for j in range(i + 1, len(arr)):
+            # Select the smallest value
+            if arr[j] < arr[minimum]:
+                minimum = j
+
+        # Place it at the front of the 
+        # sorted end of the array
+        arr[minimum], arr[i] = arr[i], arr[minimum]
+            
+    return arr
