af17b2bf00de7ecceb1980634269adeac34c6fb2
Tega Ileleji <tegaileleji@Tegas-MBP.attlocal.net>
2019-12-09 23:43:49 -0800

    Constructor

---
+++ Random_code_store/OOP/Constructor.py
@@ -0,0 +1,56 @@
+#Constructor 
+
+class Human:
+   def __init__(self):
+       self.legs = 2
+       self.arms = 2
+
+bob = Human()
+print(bob.legs)
+
+
+
+
+
+
+
+
+
+class Plane:
+    def __init__(self):
+        self.wings = 2
+
+        # fly
+        self.drive()
+        self.flaps()
+        self.wheels()
+
+    def drive(self):
+            print('Accelerating')
+
+    def flaps(self):
+            print('Changing flaps')
+
+    def wheels(self):
+            print('Closing wheels')
+
+ba = Plane()
+
+
+
+
+
+class Bug:
+   def __init__(self):
+       self.wings = 4
+
+class Human:
+   def __init__(self):
+       self.legs = 2
+       self.arms = 2
+
+bob = Human()
+tom = Bug()
+
+print(tom.wings)
+print(bob.arms)
