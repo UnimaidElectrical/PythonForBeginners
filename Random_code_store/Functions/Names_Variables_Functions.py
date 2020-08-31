88874e02e5569ac344be8cad4579c95ea4c664f0
Tega Tega <tegaslink@gmail.com>
2020-08-31 13:29:54 -0700

    nudge = 1

--- Random_code_store/Functions/Names_Variables_Functions.py
+++ Random_code_store/Functions/Names_Variables_Functions.py
@@ -105,3 +105,199 @@ print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
 print("Here is a puzzle.")
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 print("That becomes: ", what, "Can you do it by hand?")
+
+
+
+# contains(arr, value): return True if value is in arr
+
+# def arrayCheck(arr, value):
+    
+
+"""
+More Functions Pratice
+"""
+
+print("Let's pratice everything")
+print('You\'d need to know \'bout escapes with \\ that do:')
+print('\nnewlines and \t tabs')
+
+poem = """
+\t The lovely world
+with logic so firmly planted
+cannot desern \nthe needs of love
+nor comprehend position from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
+
+print("----------")
+print(poem)
+print("----------")
+
+
+
+five = 10-2+3-6
+print(f"This should be five: {five} ")
+
+def secret_formular(started):
+    jelly_beans = started * 500
+    jars = jelly_beans /1000
+    crates = jars /100
+    return jelly_beans, jars, crates
+
+
+start_point = 10000
+beans, jars, crates = secret_formular(start_point)
+
+#remember that this is another way to format a string
+print("with a starting point of: {}".format(start_point))
+# it's just like with an f"" string
+print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
+
+start_point = start_point /10
+
+
+print("We can also do that this way:")
+
+formular = secret_formular(start_point)
+#This is an easy way to apply a list to a format string
+
+print("We'd have {}beans, {} Jars, and {} crates.".format(*formular))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# faster than a plane of alabama bbq
+
+
+
+
+# def knockout(matedor):
+#     calling_another_function(mistah, misiz, omomor)
+#     firstName = scales + 300
+#     secondName = firstName + 400
+#     thirdName = secondName + 500
+#     return firstName, secondName, thirdName
+
+
+# def calling_another_function(mistah, misiz, omomor):
+#     mistah = 'a'
+#     misiz = 'b'
+#     omomor = 'c'
+#     return calling_another_function(mistah,misiz,omomor)
+
+
+# knockout(20)
+
+
+
+#Python Reserved words
+and       |  del       |  for       |  is       |  raise   
+assert    |  elif      |  from      |  Lamda    |  return
+break     |  else      |  global    |  not      |  try
+class     |  except    |  if        |  or       |  while
+continue  |  exec      |  import    |  pass     |  yield
+def       |  finally   |  in        |  print
+
+
+
+
+
+
+#Augumented Assignment Statements
+
+X += Y
+X *= Y
+X %= Y 
+X &= Y
+X ^= Y
+X <<= Y
+X >>= Y
+X -= Y
+X /= Y
+X **= Y
+X |= Y
+X //= Y
+
+S = "spam"
+S += "SPAM"
+S
+
+
+X=0
+Y=0
+X=5
+Y=4
+X >>= Y
+X
+Y
+
+
+#UNPACKING ASSIGNMENT
+nudge = 1
+wink = 2
+A, B = nudge, wink
+A, B
+
+[C, D] = [nudge, wink]
+C, D
+
+#SWAP
+nudge = 1
+wink = 2
+nudge, wink = wink, nudge
+nudge, wink
+
+#Range Assignment
+
+red, green, blue = range(3)
+red, blue
+
+
+
+
+L = [1, 2]
+L = L + [3]     #Concartinate: slower
+L
+
+
+L.append[4]    #Faster, but in-place
+L
+
+
+L = L + [5, 6]      #Concartinate: Slower
+L
+
+L.extend([7, 8])      #Faster, but in-place
+L
+
+#The inplace equavalent obviously runs faster than the concartinate option 
+
+L += [9, 10]  #Mapped to L.extend([9, 10])
+L
+
+L[len(L):]  = [11,12,13]   #this is the same thing as extend 
+
+
+
+
+
+ 
+
+
+
+
+
+
+
