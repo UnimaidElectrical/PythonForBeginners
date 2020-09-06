50bf5c1127803b4d8b966aff4596bfd1d1f512ed
Tega Tega <tegaslink@gmail.com>
2020-09-06 01:46:56 -0400

    While Loops

--- Random_code_store/If_Else_Statement/while_loops.py
+++ Random_code_store/If_Else_Statement/while_loops.py
@@ -20,3 +20,45 @@ print("The numbers: ")
 
 for num in numbers:
     print(num)
+
+print(numbers)
+
+
+#code to print all even numbers
+x=10
+while x:
+    x-=1
+    if x % 2 != 0: continue
+    print(x)
+
+
+while True:
+    name = input('Enter name: ')
+    if name == 'stop': break
+    age = int(input('Enter Age: '))  # You can have the int on theis line  
+    print('Hello' , name, '=>',  int(age)**2) # or on this line. # you can also choose to use commas 
+    #print('Hello' + name + 'of names' '=>'+  int(age)**2) # you may run into issues using concartinate here. As python only concartinates strings not int
+
+************************************************
+
+y = int(input('Enter your Number here: '))
+m = y / 2
+while m>1:
+    if y % m == 0:
+        print(y, 'has factor', m)
+        break
+    x -= 1
+else:
+    print(y, 'is prime')
+
+
+#or this as well
+
+y = int(input('Enter your Number here: ')); x = y / 2
+while x>1:
+    if y % x ==0:
+        print(y, 'has factor', x)
+        break
+    x -= 1
+else:
+    print(y, 'is prime')
