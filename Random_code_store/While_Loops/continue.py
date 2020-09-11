6facc8889abcdae305b0814d14e37bc5938b1710
Tega Tega <tegaslink@gmail.com>
2020-09-11 02:11:32 -0400

    continue statement

--- Random_code_store/While_Loops/continue.py
+++ Random_code_store/While_Loops/continue.py
@@ -11,3 +11,25 @@ while True:
         break
     print(i)
 print("Finished")
+
+
+
+'''
+The continue statement in Python returns the control to the beginning of the current loop. 
+When encountered, the loop starts next iteration without executing the remaining statements in the current iteration.
+'''
+#The continue statement can be used in both while and for loops.
+
+for letter in 'Python':     # First Example
+   if letter == 'h':
+      continue
+   print ('Current Letter :', letter)
+
+var = 10                    # Second Example
+while var > 0:              
+   var -=1
+   if var == 5:
+      continue
+   print ('Current variable value :', var)
+print ("Good bye!")
+
