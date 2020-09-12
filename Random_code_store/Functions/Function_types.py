e8354f0db80b09715df66a5698077011f5eeac03
Tega Tega <tegaslink@gmail.com>
2020-09-12 16:28:46 -0400

    Function Types

--- Random_code_store/Functions/Function_types.py
+++ Random_code_store/Functions/Function_types.py
@@ -142,3 +142,34 @@ x, y = multiple(x,y)
 x, L 
 x,y
 
+
+
+
+def func(*args):
+    print(args)
+
+func()
+func('a'=1, 'b'=2)
+
+
+
+def func(a, *pargs, **kargs):
+    print(a , pargs, kargs)
+
+func(1,2,3,x=1,y=2)
+
+
+
+Combining Keywords and Defaults
+
+def func(spam, eggs, toast=0, ham=0): #First 2 required
+    print(spam,eggs, toast,ham)
+
+func(1,2)                       #Output: (1,2,0,0)
+func(1,ham=1, eggs=0)           #Output: (1,0,0,1)
+func(spam=1, eggs=0)            #Output: (1,0,0,0)
+func(toast=1,eggs=2,spam=3)     #Output: (3,2,1,0)
+func(1,2,3,4)                   #Output: (1,2,3,4)
+
+
+
